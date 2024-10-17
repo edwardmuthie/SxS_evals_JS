6de3d2b7115d4f66f0b1f3e9df1da9a0f281792b
edwardmuthie <edward.g@turing.com>
2024-10-17 21:48:08 +0300

    203276_modelB

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import moment from 'moment';
 import 'moment/locale/es'; // Import Spanish locale
 
-moment.locale('es');
+moment.locale('es'); 
 
 const date = moment([2024, 6, 3]); // July 3rd, 2024
 
-console.log(date.format('LL')); // Output: 3 de julio de 2024
+console.log(date.format('LL'));   // Output: 3 de julio de 2024
 console.log(date.format('LLLL')); // Output: miércoles, 3 de julio de 2024 0:00
